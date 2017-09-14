51650f7fe69d77eac3b759c8f6c91bee8cc52abc
Munsterberg <jetrussell93@gmail.com>
2017-09-14 18:03:28 -0400

    create basic redux setup

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import configureStore from './store/configureStore';
 import AppRouter from './routers/AppRouter';
+
+import { addExpense } from './actions/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
+const store = configureStore();
+
+store.dispatch(addExpense({ description: 'Waterbill' }));
+
+console.log(store.getState());
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -15,7 +15,6 @@ function Layout(props) {
       {props.children}
       <Footer />
     </div>
-
   );
 }
 
